48734313b135598f1aba87376de0b225c2c9c46c
evgeny_sem163 <evg82tol@gmail.com>
2023-04-15 12:56:36 +0300

    StreamApi

---
+++ java/com/employeeBookOne/EmployeeStrimApi/EmployeeStrimApiApplication.java
@@ -0,0 +1,13 @@
+package com.employeeBookOne.EmployeeStrimApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeStrimApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeStrimApiApplication.class, args);
+	}
+
+}
---
+++ java/com/employeeBookOne/EmployeeStrimApi/controller/EmployeeController.java
@@ -0,0 +1,53 @@
+package com.employeeBookOne.EmployeeStrimApi.controller;
+
+import com.employeeBookOne.EmployeeStrimApi.employeeservice.EmployeeService;
+import com.employeeBookOne.EmployeeStrimApi.exeptions.NotFindException;
+import com.employeeBookOne.EmployeeStrimApi.models.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+    @RequestMapping("/departments")
+    public class EmployeeController {
+        private final EmployeeService employeeService;
+
+        public EmployeeController(EmployeeService employeeService) {
+            this.employeeService = employeeService;
+        }
+
+        @GetMapping("/add")
+        public Employee addEmployee(@RequestParam("firstName") String firstName,
+                                    @RequestParam("lastName") String lastName,
+                                    @RequestParam("salary") int salary, @RequestParam("department")
+                                    int department) {
+            return employeeService.addEmployee(
+                    new Employee(firstName, lastName, salary, department));
+        }
+
+        @GetMapping("/max-salary")
+        public Employee findMaxSalary(@RequestParam("departmentId")
+                                      int department) throws NotFindException {
+            return employeeService.findMaxSalary(department);
+        }
+
+        @GetMapping("/min-salary")
+        public Employee findMinSalary(@RequestParam("departmentId")
+                                      int department) throws NotFindException {
+            return employeeService.findMinSalary(department);
+        }
+
+        @GetMapping("/all")
+        public List<Employee> findEmployeeFromDepartment(@RequestParam(value = "/departmentId",
+                required = false) Integer department) {
+            if (department != null) {
+                return employeeService.findEmployeeFromDepartment(department);
+            } else {
+                return employeeService.findEmployeeAllFromDepartment();
+            }
+        }
+    }
+

d2f46dc4ae4ce63cba80dedf1d034ce9bf912101
evgeny_sem163 <evg82tol@gmail.com>
2023-04-17 22:08:05 +0300

    Готово, просьба проверить

--- java/com/employeeBookOne/EmployeeStrimApi/EmployeeStrimApiApplication.java
+++ java/com/employeeBookOne/EmployeeStrimApi/EmployeeStrimApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class  EmployeeStrimApiApplication {
+public class EmployeeStrimApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeeStrimApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeStrimApiApplication.class, args);
+    }
 
 }
